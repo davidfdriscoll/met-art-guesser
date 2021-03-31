6e8e9a517a1545d4f3cdd0b2aedcf610dabc22a7
davidfdriscoll <david.f.driscoll@gmail.com>
2021-03-31 15:24:33 -0700

    Removed unused classes from infodisplay

--- src/components/atoms/InfoDisplay.js
+++ src/components/atoms/InfoDisplay.js
@@ -1,18 +1,9 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
 
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles((theme) => ({
-  score: {
-    alignSelf: 'flex-end',
-  },
-}));
-
 export default function InfoDisplay(props) {
-  const classes = useStyles();
-
   return(
     <Box display="flex" justifyContent="space-around">
       <Typography variant="overline">
