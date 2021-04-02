c63b15605c05c2cec08e03ec1cb184d68c2b89e3
davidfdriscoll <david.f.driscoll@gmail.com>
2021-04-02 15:18:20 -0700

    Created white label on slider thumb
    Closes #2

--- src/components/atoms/GuessSlider.js
+++ src/components/atoms/GuessSlider.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { withStyles, makeStyles, useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 import Hidden from '@material-ui/core/Hidden';
+
+import ValueLabel from "@material-ui/core/Slider/ValueLabel";
+
+import { marksFull, marksMobile } from '../../data/Marks';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,82 +22,7 @@ const useStyles = makeStyles((theme) => ({
       paddingRight: theme.spacing(6),
     },    
   },
-  slider: {
-    valueLabel: {
-      color: theme.palette.common.white,
-    },
-  },
 }));
-
-const marksFull = [
-  {
-    value: -500,
-    label: '500 BCE or earlier',
-  },
-  { value: -400, },
-  { value: -300, },
-  { value: -200, },
-  { value: -100, },
-  {
-    value: 1,
-    label: '1 CE',
-  },
-  { value: 100, },
-  { value: 200, },
-  { value: 300, },
-  { value: 400, },
-  {
-    value: 500,
-    label: '500 CE',
-  },
-  { value: 600, },
-  { value: 700, },
-  { value: 800, },
-  { value: 900, },
-  {
-    value: 1000,
-    label: '1000 CE',
-  },
-  { value: 1100, },
-  { value: 1200, },
-  { value: 1300, },
-  { value: 1400, },
-  {
-    value: 1500,
-    label: '1500 CE',
-  },
-  { value: 1600, },
-  { value: 1700, },
-  { value: 1800, },
-  { value: 1900, },
-  {
-    value: 2000,
-    label: '2000 CE',
-  },
-];
-
-const marksMobile = [
-  {
-    value: -500,
-    label: '<500 BCE',
-  },
-  {
-    value: 1,
-  },
-  {
-    value: 500,
-  },
-  {
-    value: 1000,
-  },
-  {
-    value: 1500,
-    label: '1500 CE',
-  },
-  {
-    value: 2000,
-  },
-];
 
 function valuetext(value) {
   return value > 0 ? `${value}` : `${-value}`;
@@ -103,6 +32,12 @@ export default function GuessSlider(props) {
   const classes = useStyles();
   const theme = useTheme();
   const largerScreen = useMediaQuery(theme.breakpoints.up('sm'));
+
+  const WhiteValueLabel = withStyles({
+    label: {
+      color: theme.palette.common.white,
+    }
+  })(ValueLabel);
 
   return(
     <Box className={classes.root}>
@@ -120,6 +55,7 @@ export default function GuessSlider(props) {
         aria-labelledby="guess-slider"
         valueLabelDisplay="on"
         valueLabelFormat={valuetext}
+        ValueLabelComponent={WhiteValueLabel}
         marks={largerScreen ? marksFull : marksMobile}
         track={false}
         className={classes.slider}
---
+++ src/data/Marks.js
@@ -0,0 +1,69 @@
+export const marksFull = [
+  {
+    value: -500,
+    label: '500 BCE or earlier',
+  },
+  { value: -400, },
+  { value: -300, },
+  { value: -200, },
+  { value: -100, },
+  {
+    value: 1,
+    label: '1 CE',
+  },
+  { value: 100, },
+  { value: 200, },
+  { value: 300, },
+  { value: 400, },
+  {
+    value: 500,
+    label: '500 CE',
+  },
+  { value: 600, },
+  { value: 700, },
+  { value: 800, },
+  { value: 900, },
+  {
+    value: 1000,
+    label: '1000 CE',
+  },
+  { value: 1100, },
+  { value: 1200, },
+  { value: 1300, },
+  { value: 1400, },
+  {
+    value: 1500,
+    label: '1500 CE',
+  },
+  { value: 1600, },
+  { value: 1700, },
+  { value: 1800, },
+  { value: 1900, },
+  {
+    value: 2000,
+    label: '2000 CE',
+  },
+];
+
+export const marksMobile = [
+  {
+    value: -500,
+    label: '<500 BCE',
+  },
+  {
+    value: 1,
+  },
+  {
+    value: 500,
+  },
+  {
+    value: 1000,
+  },
+  {
+    value: 1500,
+    label: '1500 CE',
+  },
+  {
+    value: 2000,
+  },
+];
